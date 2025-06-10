d298d1c38d0a5d53b869962ba6ef416f76e9282e
Dennis Tereshkow <tereshkowdennis@gmail.com>
2025-06-10 22:27:24 +0300

    fix: update testing urls

--- alcoteka_scraper/alkoteka_scraper/constants.py
+++ alcoteka_scraper/alkoteka_scraper/constants.py
@@ -4,6 +4,6 @@ ALLOWED_DOMAINS = ["alkoteka.com"]
 
 START_URLS = [
     'https://alkoteka.com/catalog/vino',
-    # 'https://alkoteka.com/catalog/slaboalkogolnye-napitki-2',
-    # 'https://alkoteka.com/catalog/krepkiy-alkogol',
+    'https://alkoteka.com/catalog/slaboalkogolnye-napitki-2',
+    'https://alkoteka.com/catalog/krepkiy-alkogol',
 ]
